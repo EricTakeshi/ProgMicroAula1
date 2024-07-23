35d30bf672f12e2ee11653978e450382408e37a7
Eric Takeshi <zenke.takeshi.eric@uel.br>
2024-07-23 12:00:00 -0300

    função de blink e fall adicionadas

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -69,7 +69,13 @@ typedef struct
 	uint16_t GPIO_Pin;
 	stimer dbtimer;
 }sBot;
-
+typedef struct
+{
+	stimer bkrtimer;
+	GPIO_TypeDef* GPIOx;
+	uint16_t GPIO_Pin;
+	
+}sblinker;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -96,6 +102,8 @@ typeTimerStatus stimer_is_off(stimer* pTimer);
 typeBoolStatus is_Rise_Edge(sBot* pBot);
 void edge_detect_init(sBot* pBot, GPIO_TypeDef* GPIOz, uint16_t zPin, typeEdge edge, uint32_t dbouncing);
 typeBoolStatus is_Fall_Edge(sBot* pBot);
+void blinker_init(sblinker* pBlinker, GPIO_TypeDef* GPIOx, uint16_t xPin, uint32_t hfper);
+void blinker_function(sblinker* pBlinker);
 
 /* USER CODE END PFP */
 
@@ -141,9 +149,13 @@ int main(void)
 	stimer_init(&stimer1, 500);
 	stimer_init(&stimer2, 1000);
 	sBot bluebot;
+	sblinker OrangeBlink;
+	sblinker BlueBlink;
 	typeBoolStatus OrangeLedBlinkOn = false;
 	
-	edge_detect_init(&bluebot,Bot_Blue_GPIO_Port, Bot_Blue_Pin, rise, 100);
+	edge_detect_init(&bluebot,Bot_Blue_GPIO_Port, Bot_Blue_Pin, fall, 150);
+	blinker_init(&OrangeBlink, LED_Orange_GPIO_Port, LED_Orange_Pin, 500);
+	blinker_init(&BlueBlink, LED_Blue_GPIO_Port, LED_Blue_Pin, 1000);
 	
 	//uint16_t bstatus_old, bstatus;
 	//bstatus = HAL_GPIO_ReadPin(Bot_Blue_GPIO_Port, Bot_Blue_Pin);
@@ -159,7 +171,7 @@ int main(void)
     /* USER CODE BEGIN 3 */
 	  
 	  
-	  if (true == is_Rise_Edge(&bluebot))
+	  if (true == is_Fall_Edge(&bluebot))
 	  {
 		  OrangeLedBlinkOn = !OrangeLedBlinkOn;
 		  a++;
@@ -167,22 +179,10 @@ int main(void)
 	  
 	  if (true == OrangeLedBlinkOn)
 	  {
-		  if (off == stimer_is_off(&stimer1))
-		  {
-			  HAL_GPIO_TogglePin(LED_Orange_GPIO_Port, LED_Orange_Pin);
-			  stimer_reload(&stimer1);
-		  }
+		  blinker_function(&OrangeBlink);
 	  }
 	 
-	  
-	  if (stimer_is_off(&stimer2))
-	  {
-		  HAL_GPIO_TogglePin(LED_Blue_GPIO_Port, LED_Blue_Pin);
-		  stimer_reload(&stimer2);
-	  }
-	  
-	  
-	  
+	  blinker_function(&BlueBlink);
 	  
   }
   /* USER CODE END 3 */
@@ -296,6 +296,46 @@ void edge_detect_init(sBot* pBot, GPIO_TypeDef* GPIOz, uint16_t zPin, typeEdge e
 		pBot->atu = release;
 	}
 	
+}
+typeBoolStatus is_Fall_Edge(sBot* pBot)
+{
+	switch (pBot->edge_detect_state)
+	{
+	case edge_detect:
+		pBot->ant = pBot->atu;
+		pBot->atu = (typePushBottonState)HAL_GPIO_ReadPin(pBot->GPIOx, pBot->GPIO_Pin);
+		if (pBot->ant == push && pBot->atu == release)
+		{
+			pBot->edge_detect_state = filter_bouncing;
+			stimer_init(&(pBot->dbtimer), pBot->dbtimer.time_lapse);
+		}
+		break;
+	case filter_bouncing:
+		if (stimer_is_off(&(pBot->dbtimer)))
+		{
+			pBot->atu = (typePushBottonState)HAL_GPIO_ReadPin(pBot->GPIOx, pBot->GPIO_Pin);
+			pBot->edge_detect_state = edge_detect;
+			return ((typeBoolStatus)!(pBot->atu));
+		}
+		break;
+	default:
+		break;
+	}
+	return (false);
+}
+void blinker_init(sblinker* pBlinker, GPIO_TypeDef* GPIOx, uint16_t xPin, uint32_t hfper)
+{
+	pBlinker->GPIOx = GPIOx;
+	pBlinker->GPIO_Pin = xPin;
+	stimer_init(&(pBlinker->bkrtimer), hfper);
+}
+void blinker_function(sblinker* pBlinker)
+{
+	if (stimer_is_off(&(pBlinker->bkrtimer)))
+	{
+		HAL_GPIO_TogglePin(pBlinker->GPIOx, pBlinker->GPIO_Pin);
+		stimer_reload(&(pBlinker->bkrtimer));
+	}
 }
 /* USER CODE END 4 */
 
